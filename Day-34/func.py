4dca5b481a7885e5e7368844a0eef01da4eeccff
Chamarthi Chandhini <95535625+chandhini4509@users.noreply.github.com>
2024-03-14 20:49:52 +0530

    msg

---
+++ Day-34/func.py
@@ -0,0 +1,15 @@
+def calculate_area(len,wid):
+    return len*wid
+
+print(calculate_area(8,5))
+person={
+    "name":"ccr",
+    "age":20,
+    "city":"hyderabad"
+}
+def print_person_info(person):
+    print(person["name"])
+    print(person["age"])
+    print(person["city"])
+print_person_info(person)
+
